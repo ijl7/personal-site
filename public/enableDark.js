0ccc91ccd1cd137d521251997c4617665b79a9c5
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-19 16:57:38 -0400

    i was checking the wrong equality

--- public/enableDark.js
+++ public/enableDark.js
@@ -1,5 +1,5 @@
 function enableDark(){
-    if(localStorage.getItem('dark-theme') == "dark-theme"){
+    if(localStorage.getItem('dark-theme') == '"dark-theme"'){
         const className = document.body.className;
         className.replace("light-theme", "dark-theme");
         document.body.className.replace(document.body.className, className);
