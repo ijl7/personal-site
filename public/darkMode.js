4567675bcabf9711c31500083171194575177995
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-19 15:17:43 -0400

    added an if statement to remove nulls

--- public/darkMode.js
+++ public/darkMode.js
@@ -1,12 +1,14 @@
 const switcher = document.querySelector('.bar');
 
-document.body.classList.toggle(localStorage.getItem('dark'));
+if(localStorage.getItem('dark-theme') != null){
+    document.body.classList.toggle(localStorage.getItem('dark-theme'));
+}
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
     const className = document.body.className;
-    localStorage.setItem('dark', JSON.stringify(className));
+    localStorage.setItem('dark-theme', JSON.stringify(className));
     if(className == "light-theme") {
         document.querySelector(".slider").style.left = "0%";
         document.querySelector('.slider').textContent = "Light";
