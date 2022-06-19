b4d87e9464059a845f24f52e350538319bf22b53
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-19 15:07:41 -0400

    worked on fixing dark mode transfer

--- public/darkMode.js
+++ public/darkMode.js
@@ -1,11 +1,12 @@
-'use strict';
 const switcher = document.querySelector('.bar');
-const className = document.body.className;
-localStorage.setItem(document.body.className, className)
+
+document.body.classList.toggle(localStorage.getItem('dark'));
+
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
     const className = document.body.className;
+    localStorage.setItem('dark', className);
     if(className == "light-theme") {
         document.querySelector(".slider").style.left = "0%";
         document.querySelector('.slider').textContent = "Light";
