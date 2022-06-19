2cbc3c0cc1a3ce83ba864992a9239f5c7a392d94
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-19 15:48:30 -0400

    made an outside function instead of inside listener

--- public/darkMode.js
+++ public/darkMode.js
@@ -1,20 +1,21 @@
 const switcher = document.querySelector('.bar');
 
 if(localStorage.getItem('dark-theme') == "dark-theme"){
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+    toggleDark();
 }
 
-switcher.addEventListener('click', function() {
+switcher.addEventListener('click', toggleDark);
+
+function toggleDark() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
     const className = document.body.className;
     localStorage.setItem('dark-theme', JSON.stringify(className));
-    if(className == "light-theme") {
+    if(localStorage.getItem('dark-theme') == "light-theme") {
         document.querySelector(".slider").style.left = "0%";
         document.querySelector('.slider').textContent = "Light";
     } else {
         document.querySelector(".slider").style.left = "50%";
         document.querySelector('.slider').textContent = "Dark";
     }
-});
+}
