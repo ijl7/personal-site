5e25d7a6847bf0c71bd748e11f51452cdbdf40ef
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-19 15:50:35 -0400

    Revert "made an outside function instead of inside listener"
    This reverts commit 2cbc3c0cc1a3ce83ba864992a9239f5c7a392d94.

--- public/darkMode.js
+++ public/darkMode.js
@@ -1,21 +1,20 @@
 const switcher = document.querySelector('.bar');
 
 if(localStorage.getItem('dark-theme') == "dark-theme"){
-    toggleDark();
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
 }
 
-switcher.addEventListener('click', toggleDark);
-
-function toggleDark() {
+switcher.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
     const className = document.body.className;
     localStorage.setItem('dark-theme', JSON.stringify(className));
-    if(localStorage.getItem('dark-theme') == "light-theme") {
+    if(className == "light-theme") {
         document.querySelector(".slider").style.left = "0%";
         document.querySelector('.slider').textContent = "Light";
     } else {
         document.querySelector(".slider").style.left = "50%";
         document.querySelector('.slider').textContent = "Dark";
     }
-}
+});
