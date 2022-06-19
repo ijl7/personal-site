703ba8a6705ac6693f1301dedb78564081e54b15
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-18 20:20:05 -0400

    trying express-basic-auth password

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const basicAuth = require("express-basic-auth");
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(basicAuth(users = {'admin': 'supersecret'}));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root :__dirname});
