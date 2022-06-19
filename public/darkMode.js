96eb6db303d13226e24ccf54acd86ebc52b6d450
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-19 15:13:58 -0400

    turned an object into a string

--- public/darkMode.js
+++ public/darkMode.js
@@ -6,7 +6,7 @@ switcher.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
     const className = document.body.className;
-    localStorage.setItem('dark', className);
+    localStorage.setItem('dark', JSON.stringify(className));
     if(className == "light-theme") {
         document.querySelector(".slider").style.left = "0%";
         document.querySelector('.slider').textContent = "Light";
