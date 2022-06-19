adb0080f3723872e59a478f45acee070da0a9176
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-19 17:18:50 -0400

    added the header to the clips page

--- public/enableDark.js
+++ public/enableDark.js
@@ -2,5 +2,7 @@ function enableDark(){
     if(localStorage.getItem('dark-theme') == '"dark-theme"'){
         document.body.classList.toggle('light-theme');
         document.body.classList.toggle('dark-theme');
+        document.querySelector(".slider").style.left = "50%";
+        document.querySelector('.slider').textContent = "Dark";
     }
 }
