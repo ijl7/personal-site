e4931635deac5619935e428b61a5760494f62ecb
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-21 14:31:55 -0400

    added the basicauth to the get instead of inside

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root :__dirname});
 });
-router.get('/clips.html', (req, res) =>{
-    router.use(basicAuth({users : {'admin': 'password'}, challenge : true}));
+router.get('/clips.html', basicAuth({users : {'admin': 'password'}, challenge : true}), (req, res) =>{
     res.sendFile('clips.html', {root :__dirname});
 })
 
