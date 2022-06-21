93b74787a308be0bfe407608fbab39b91e717424
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-06-21 14:41:14 -0400

    tried using the app instead of a router

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const basicAuth = require("express-basic-auth");
-const router = express.Router();
 
 const port = process.env.PORT || 3000;
 
@@ -11,7 +10,9 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root :__dirname});
 });
-router.get('/clips.html', basicAuth({users : {'admin': 'password'}, challenge : true}), (req, res) =>{
+
+app.get('/clips.html', basicAuth({users : {'admin': 'password'}, challenge : true}), (req, res) =>{
     res.sendFile('clips.html', {root :__dirname});
 });
+
 app.listen(port);
