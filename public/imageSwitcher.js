5134d61070e23272c66d3203d719f20ed907f3cf
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-09-22 16:25:51 -0400

    made i var instead of const

--- public/imageSwitcher.js
+++ public/imageSwitcher.js
@@ -1,6 +1,6 @@
 const picture = document.querySelector('.image');
 const myImages = ["media/Maple.jpg", "media/Easter.jpg", "media/Friends.jpg", "media/Dinner.jpg"];
-const i = 0;
+var i = 0;
 picture.addEventListener('click', function() {
     var image = document.getElementById('image');
     if(i < myImages.length)
