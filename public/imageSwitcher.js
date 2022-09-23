8913d63287f30069636dbeaa25ac09443d38f8cd
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-09-23 18:03:06 -0400

    tried to update opacity of image

--- public/imageSwitcher.js
+++ public/imageSwitcher.js
@@ -1,11 +1,20 @@
 const picture = document.querySelector('.image');
 const myImages = ["media/Maple.jpg", "media/Easter.jpg", "media/Friends.jpg", "media/Dinner.jpg"];
 var i = 0;
+var opacity = 0;
 picture.addEventListener('click', function() {
     var image = document.getElementById('image');
     if(i < myImages.length - 1)
         i++;
     else
         i = 0;
+    while(opacity > 0){
+        opacity -= .01;
+        image.style = "opacity: " + opacity;
+    }
     image.src = myImages[i];
+    while(opacity < 1){
+        opacity += .01;
+        image.style = "opacity: " + opacity;
+    }
 });
