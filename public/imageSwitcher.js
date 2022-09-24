116b6ce04eb4acc5d41dded923eff8640d6cd4e5
ijl7 <62620407+ijl7@users.noreply.github.com>
2022-09-23 20:24:15 -0400

    tried to fix image fading

--- public/imageSwitcher.js
+++ public/imageSwitcher.js
@@ -1,14 +1,13 @@
-const picture = document.querySelector('.image');
+const picture = document.getElementById('image');
 const myImages = ["media/Maple.jpg", "media/Easter.jpg", "media/Friends.jpg", "media/Dinner.jpg"];
 var i = 0;
 var opacity = 0;
 picture.addEventListener('click', function() {
-    var image = document.getElementById('image');
     if(i < myImages.length - 1)
         i++;
     else
         i = 0;
-    image.classList.remove = opacity;
-    setTimeout(() => {image.src = myImages[i];}, 1000);
-    setTimeout(() => {image.classList.add = opacity;}, 2000);
+    picture.classList.remove = opacity;
+    setTimeout(() => {picture.src = myImages[i];}, 1000);
+    setTimeout(() => {picture.classList.add = opacity;}, 2000);
 });
